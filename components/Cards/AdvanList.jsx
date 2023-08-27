cb46ab2263a74664ac79c81211fbef7b18928103
rflishrl <rafliworkspace@gmail.com>
2023-08-27 19:28:50 +0700

    refactor: code

--- components/Cards/AdvanList.jsx
+++ components/Cards/AdvanList.jsx
@@ -4,9 +4,9 @@ import AdvanCard from "./AdvanCard";
 const AdvanList = ({ data }) => {
   return (
     <div className="flex flex-wrap -m-4">
-      {data.map((data, index) => (
+      {data.map((data) => (
         <AdvanCard
-          key={index}
+          key={data.id}
           icon={data.icon}
           title={data.title}
           description={data.description}
--- components/Cards/DataList.jsx
+++ components/Cards/DataList.jsx
@@ -4,9 +4,9 @@ import DataCard from "./DataCard";
 const DataList = ({ data }) => {
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 gap-4">
-      {data.map((item, index) => (
+      {data.map((item) => (
         <DataCard
-          key={index}
+          key={item.id}
           icon={item.icon}
           title={item.title}
           description={item.description}
